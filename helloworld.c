1c52ecb85cbc3ef23dd4c74b09333559e24265ce
dx <dx@nuc>
2024-01-29 16:07:49 +0800

    Call addnum

--- helloworld.c
+++ helloworld.c
@@ -3,6 +3,9 @@
 int addnum(int, int);
 
 int main(int argc, char *argv[]) {
+    int result;
     printf("Hello, World!\n");
+    result = addnum(3, 4);
+    printf("result = %d\n", result);
     return 0;
 }
